b61de8516c354569b9ff36406bca6150953fc737
Devika-H <72243421+Devika-H@users.noreply.github.com>
2022-10-15 23:18:43 +0530

    Update Area Of Triangle.c

--- Area Of Triangle.c
+++ Area Of Triangle.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 void main(){ 
-    float a, b, h;
-    printf("Enter the base : ");
-    scanf("%f", &b);
-    printf("Enter the height : ");
-    scanf("%f", &h);
-    a = 0.5 * b * h;
-    printf("Area is %f", a);
+    float b, h;
+    printf("Enter the base & height : ");
+    scanf("%f &f", &b, &h);
+    printf("Area is %f", (0.5 * b * h));
 }
